8a1b90d29308064ab1e22ca80a8b37c31d6019a0
Palma Sistemas ltda <mario@tyr.digital>
2025-03-05 17:25:51 -0300

    upt

--- Server.js
+++ Server.js
@@ -14,30 +14,33 @@ const ftpConfig = {
     host: process.env.FTP_HOST, //Servidor FTP
     user: process.env.FTP_USER, //Usuário
     password: process.env.FTP_PASSWORD, //Senha
-    secure: true
+    secure: false,
+    secureOptions: {
+        rejectUnauthorized: true
+    }
 };
 
 //Rota para Enviar arquivo
-app.post('/UPLOAD', multer({dest: '/tmp'}).single('file'), async(req, res) => {
-    if(!req.file){
-        return res.status(400).json({error: "Nenhum Arquivo Enviado!"});
+app.post('/UPLOAD', multer({ dest: '/tmp' }).single('file'), async (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: "Nenhum Arquivo Enviado!" });
     }
     const client = new ftp.Client();
     client.ftp.verbose = true;
-    try{
+    try {
         await client.access(ftpConfig);
         const remotePath = 'www/Palma/';
         await client.uploadFrom(req.file.path, remotePath + req.file.originalname);
         fs.unlink(req.file.path);
-        res.status(200).json({message: "Arquivo Enviado com Sucesso!"});
+        res.status(200).json({ message: "Arquivo Enviado com Sucesso!" });
     }
-    catch(error){
+    catch (error) {
         console.error("Erro ao Enviar ao Servidor FTP: ", error);
-        res.status(500).json({error: "Erro ao Enviar o Arquivo"});
+        res.status(500).json({ error: "Erro ao Enviar o Arquivo" });
     }
-    finally{
+    finally {
         client.close();
-    }    
+    }
 });
 
 app.listen(port, () => {
