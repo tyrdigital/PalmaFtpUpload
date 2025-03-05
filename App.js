3bdebfcbbbae15e4e0b7cda725ad7b2d3350613a
Palma Sistemas ltda <mario@tyr.digital>
2025-03-05 14:46:30 -0300

    upt

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput, BackHandler, Alert, ActivityIndicator } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
+import * as DocumentPicker from 'expo-document-picker';
+import axios from 'axios';
+
+const SERVER_CONFIG = { ip: '192.168.0.100', port: 3000 }
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [text, setText] = useState({ title: '', content: '' });
+  const [files, setFiles] = useState([]);
+
+  const FetchClipboardText = async () => { //Função 'FetchClipboardText', utilizada para pegar o conteúdo da área de transferência
+    const text = await Clipboard.getStringAsync(); //Pega Texto da Área de Transferência
+    const [text1, text2] = text.split('@@'); //Separa o conteúdo antes e depois de '@@'
+
+    setText({ title: text1, content: text2 }); //Registra cada um em 'text.title' e em 'text.content'
+  }
+
+  const HandleCancel = () => { //Função 'HandleCancel'
+    setText({ title: '', content: '' }); //Apaga 'text'
+    BackHandler.exitApp(); //Sai do aplicativo
+  }
+
+  const HandleOk = () => { //Função 'HandleOk'
+    if (files.length === 0) { //Se não tiverem arquivos
+      Alert.alert("Erro!", "Nenhum Arquivo Selecionado");
+      return;
+    }
+    else { //Senão
+      setLoading(true); //Ativa carregamento
+      const uploads = files.map((file) => UploadFile(file));
+      Promise.all(uploads)
+        .then(() => {
+          Alert.alert("Sucesso!", "Todos os Arquivos Foram Enviados com Sucesso!");
+        })
+        .catch((error) => {
+          Alert.alert("Erro!", "Erro ao Enviar Arquivos: " + error.message);
+        })
+        .finally(() => {
+          setFiles([]); //Apaga 'files'
+          setLoading(false); //Desativa carregamento
+        });
+    }
+  }
+
+  const PickFiles = async () => { //Função 'PickFiles', utilizada para selecionar arquivos para envio
+    try {
+      const result = await DocumentPicker.getDocumentAsync({
+        multiple: true //Selecionar múltiplos arquivos
+      });
+
+      if (result.canceled) { //Se o usuário cancelar
+        return; //Não faz nada
+      }
+      setFiles(result.assets); //Salva os arquivos selecionados em 'files'
+    }
+    catch (error) {
+      console.error("Erro ao Selecionar Arquivos: ", error);
+      Alert.alert("Erro ao Selecionar Arquivos!", error);
+    }
+  }
+
+  const UploadFile = async (file) => { //Função 'UploadFiles'
+    try {
+      //Formata mensagem
+      const formData = new FormData();
+      formData.append('file', {
+        uri: file.uri,
+        name: text.content + file.name.split('/').pop(),
+        type: 'application/octet-stream'
+      });
+      //Envia para o servidor
+      await axios.post(`http://${SERVER_CONFIG.ip}:${SERVER_CONFIG.port}/UPLOAD`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+        }
+      );
+    }
+    catch (error) {
+      Alert.alert("Erro!", error.message);
+    }
+  }
+
+  useEffect(() => {
+    FetchClipboardText(); //Pega o conteúdo da Área de Transferência ao iniciar o Aplicativo
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {loading ? ( //Se estiver carregando
+        <>
+          <Text style={{fontSize: 18}}>ENVIANDO ARQUIVOS</Text>
+          <ActivityIndicator size='large' color='#0000ff' style={{ marginTop: 20 }} />
+        </>
+      ) : ( //Senão
+        <>
+          <View style={styles.containerA}> {/*TÍTULO*/}
+            <TouchableOpacity style={styles.buttonContainer} onPress={HandleCancel}>
+              <Image
+                style={styles.imageContainer}
+                source={require('./assets/BotaoCancelar.jpeg')}
+              />
+            </TouchableOpacity>
+            <TextInput style={styles.inputContainer} editable={false} value={text.title} />
+            <TouchableOpacity style={styles.buttonContainer} onPress={HandleOk}>
+              <Image
+                style={styles.imageContainer}
+                source={require('./assets/BotaoOk.jpeg')}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.containerB}> {/*CORPO*/}
+            <Text>ARQUIVOS:</Text>
+            <TouchableOpacity style={[styles.inputContainer, { width: '100%', height: '10%' }]} onPress={PickFiles}>
+              <TextInput editable={false} value={files.map((file) => text.content + file.name).join(',')} />
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  //Containeres PRIMÁRIOS
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' },
+  containerA: { width: '100%', height: '10%', padding: 5, gap: 5, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' },
+  containerB: { width: '100%', height: '90%', padding: 5, gap: 5, flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start' },
+  //Containeres SECUNDÁRIOS
+  buttonContainer: { width: '18%', height: '100%', borderWidth: 2, justifyContent: 'center', alignItems: 'center' },
+  imageContainer: { width: '100%', height: '100%', resizeMode: 'contain' },
+  inputContainer: { width: '60%', height: '100%', borderWidth: 2, borderRadius: 3, fontSize: 18, alignItems: 'center', justifyContent: 'center', textAlign: 'center' }
 });
---
+++ Server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const ftp = require('basic-ftp'); // Importa diretamente o módulo FTP
+const { error } = require('console');
+const app = express();
+app.use(cors()); //Permitir requisições de outras origens
+const port = 3000;
+
+const SERVER = {
+    host: 'www.palmasistemas.com.br', //Servidor FTP
+    user: 'palmasistemas', //Usuário
+    password: 'gremio1983', //Senha     
+}
+
+//Informações do servidor FTP
+const ftpConfig = {
+    host: SERVER.host, //Servidor FTP
+    user: SERVER.user, //Usuário
+    password: SERVER.password, //Senha
+    secure: true,
+    secureOptions: {
+        rejectUnauthorized: false // Ignorar a verificação do certificado
+    }
+};
+
+//Definir diretório onde onde os arquivos serão salvos
+const uploadDirectory = path.join(__dirname, 'Palma');
+
+//Configurar o multer para armazenar os arquivos na pasta
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDirectory); //Caminho do diretório
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname); //Obtém a extensão do arquivo
+        const filename = file.originalname; //Define o nome do arquivo com timestamp
+        cb(null, filename); //Define o nome do arquivo
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/UPLOAD', upload.single('file'), async (req, res) => { //Rota para receber arquivo
+    if (!req.file) {
+        return res.status(400).json({ error: "Nenhum Arquivo Enviado!" });
+    }
+    try {
+        const client = new ftp.Client(); //Cria o cliente FTP
+        client.ftp.verbose = true;
+        await client.access(ftpConfig); // Conecta-se ao servidor FTP
+        const remotePath = '/www/Palma/'; // Caminho da pasta de destino no servidor FTP
+        await client.uploadFrom(req.file.path, remotePath + req.file.filename); // Faz o upload do arquivo para o servidor FTP
+        fs.unlinkSync(req.file.path); // Após o upload, remove o arquivo temporário do servidor local
+        res.status(200).json({ message: 'Arquivo enviado para o FTP com sucesso!', file: req.file }); // Responde ao cliente
+        client.close(); // Fecha a conexão FTP
+    }
+    catch (error) {
+        console.error('Erro ao enviar para o FTP:', error);
+        res.status(500).json({ error: 'Falha ao enviar o arquivo para o servidor FTP' });
+    }
+});
+
+//Cria a pasta(Se não existir)
+if (!fs.existsSync(uploadDirectory)) {
+    fs.mkdirSync(uploadDirectory);
+}
+
+app.listen(port, () => {
+    console.log("Servidor rodando!");
+});
