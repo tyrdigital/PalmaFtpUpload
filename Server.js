59a2223282bde5dac7d1ec321b0fed9df734d8e5
Palma Sistemas ltda <mario@tyr.digital>
2025-03-06 16:03:22 -0300

    upt

--- Server.js
+++ Server.js
@@ -50,7 +50,7 @@ app.post('/UPLOAD', upload.single('file'), async (req, res) => { //Rota para rec
     console.log('Arquivo recebido:', req.file);
     const client = new ftp.Client(); //Cria o cliente FTP
     client.ftp.verbose = true;
-    client.ftp.usePassive(true); // Forçar o modo passivo
+    client.ftp.setPassive(true); // Forçar o modo passivo
     try {
         console.log('Tentando conectar ao FTP...');
         await client.access(ftpConfig); // Conecta-se ao servidor FTP
